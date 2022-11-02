398dff34ec6704b660720da9a2b2841ce2d4e64b
atulmahindra <jadhav.atul3@mahindra.com>
2022-11-02 13:55:35 +0530

    LOGIN ONLY

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-import { SignupComponent } from './signup/signup.component';
+
 import { LoginComponent } from './login/login.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatInputModule} from '@angular/material/input';
@@ -13,7 +13,6 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    SignupComponent,
     LoginComponent
   ],
   imports: [
--- src/app/cms/cms-routing.module.ts
+++ src/app/cms/cms-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SignupComponent } from '../auth/signup/signup.component';
+
 import { LoginComponent } from '../auth/login/login.component';
 
 const routes: Routes = [
   
   { path: '',component: LoginComponent },
-  { path: 'signup',component: SignupComponent },
   { path: '404', component: PageNotFoundComponent }
 ];
 
